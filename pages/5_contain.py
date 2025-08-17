9d690d42a25b8d52f8c08230958aa4bc976fcbea
Olaitan Toluwanimi Michael <101925362+Theomnitron@users.noreply.github.com>
2025-08-17 22:55:35 +0000

    dd

--- pages/5_contain.py
+++ pages/5_contain.py
@@ -179,19 +179,19 @@
 #             unsafe_allow_html=True
 #         )
 
-# import streamlit as st
+import streamlit as st
 
-# message = st.text_area("Message", value="Lorem ipsum.\nStreamlit is cool.")
+message = st.text_area("Message", value="Lorem ipsum.\nStreamlit is cool.")
 
-# if st.button("Prepare download"):
-#     st.download_button(
-#         label="Download text",
-#         data=message,
-#         file_name="message.pdf",
-#         on_click="ignore",
-#         type="primary",
-#         icon=":material/download:",
-#     )
+if st.button("Prepare download"):
+    st.download_button(
+        label="Download text",
+        data=message,
+        file_name="message.txt",
+        on_click="ignore",
+        type="primary",
+        icon=":material/download:",
+    )
 
 # import streamlit as st
 # from pdf2docx import Converter
@@ -241,25 +241,25 @@
 # components.html(show_print_button)
 
 
-import streamlit as st
-from fpdf import FPDF
-import base64
+# import streamlit as st
+# from fpdf import FPDF
+# import base64
 
-report_text = st.text_input("Report Text")
+# report_text = st.text_input("Report Text")
 
 
-export_as_pdf = st.button("Export Report")
+# export_as_pdf = st.button("Export Report")
 
-def create_download_link(val, filename):
-    b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+# def create_download_link(val, filename):
+#     b64 = base64.b64encode(val)  # val looks like b'...'
+#     return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
-if export_as_pdf:
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font('Arial', 'B', 16)
-    pdf.cell(40, 10, report_text)
+# if export_as_pdf:
+#     pdf = FPDF()
+#     pdf.add_page()
+#     pdf.set_font('Arial', 'B', 16)
+#     pdf.cell(40, 10, report_text)
     
-    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+#     html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
 
-    st.markdown(html, unsafe_allow_html=True)
+#     st.markdown(html, unsafe_allow_html=True)
