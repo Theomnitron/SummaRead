4ce0a5cf9a46f96842d15f4c1fbd155741ca8baa
Olaitan Toluwanimi Michael <101925362+Theomnitron@users.noreply.github.com>
2025-08-17 19:13:57 +0000

    contain

--- pages/5_contain.py
+++ pages/5_contain.py
@@ -26,11 +26,15 @@ with st.container(border= True):
          Later, she was captured, converted to Christianity, and married John Rolfe, an English tobacco planter, and traveled to England, where she became a celebrity.
          She died in England in 1617, at the young age of 20 or 21.
          She is famously known for her interactions with the English colonists at Jamestown.
+         While popular culture often depicts a romantic relationship with John Smith, historians debate the specifics of their interactions, suggesting a more complex dynamic involving diplomacy, cultural exchange, and potential conflict. 
+         """)
+    st.write('---')
+    st.write("""
+        Pocahontas, born around 1596, was a Native American woman from the Powhatan tribe in Virginia.
+         She is famously known for her interactions with the English colonists at Jamestown.
          While popular culture often depicts a romantic relationship with John Smith, historians debate the specifics of their interactions, suggesting a more complex dynamic involving diplomacy, cultural exchange, and potential conflict.
          Later, she was captured, converted to Christianity, and married John Rolfe, an English tobacco planter, and traveled to England, where she became a celebrity.
          She died in England in 1617, at the young age of 20 or 21.
          She is famously known for her interactions with the English colonists at Jamestown.
-         While popular culture often depicts a romantic relationship with John Smith, historians debate the specifics of their interactions, suggesting a more complex dynamic involving diplomacy, cultural exchange, and potential conflict.
-         Later, she was captured, converted to Christianity, and married John Rolfe, an English tobacco planter, and traveled to England, where she became a celebrity.
-         She died in England in 1617, at the young age of 20 or 21. 
+         While popular culture often depicts a romantic relationship with John Smith, historians debate the specifics of their interactions, suggesting a more complex dynamic involving diplomacy, cultural exchange, and potential conflict. 
          """)
