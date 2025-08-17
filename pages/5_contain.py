1487f688c3c1570f90a9ddc5dfc8c66d9a28d3f6
Olaitan Toluwanimi Michael <101925362+Theomnitron@users.noreply.github.com>
2025-08-17 19:16:52 +0000

    summary strct

--- pages/5_contain.py
+++ pages/5_contain.py
@@ -19,15 +19,7 @@ st.write("""
 st.write('---')
 
 with st.container(border= True):
-    st.write("""
-        Pocahontas, born around 1596, was a Native American woman from the Powhatan tribe in Virginia.
-         She is famously known for her interactions with the English colonists at Jamestown.
-         While popular culture often depicts a romantic relationship with John Smith, historians debate the specifics of their interactions, suggesting a more complex dynamic involving diplomacy, cultural exchange, and potential conflict.
-         Later, she was captured, converted to Christianity, and married John Rolfe, an English tobacco planter, and traveled to England, where she became a celebrity.
-         She died in England in 1617, at the young age of 20 or 21.
-         She is famously known for her interactions with the English colonists at Jamestown.
-         While popular culture often depicts a romantic relationship with John Smith, historians debate the specifics of their interactions, suggesting a more complex dynamic involving diplomacy, cultural exchange, and potential conflict. 
-         """)
+    st.title("Hello There!")
     st.write('---')
     st.write("""
         Pocahontas, born around 1596, was a Native American woman from the Powhatan tribe in Virginia.
@@ -38,3 +30,14 @@ with st.container(border= True):
          She is famously known for her interactions with the English colonists at Jamestown.
          While popular culture often depicts a romantic relationship with John Smith, historians debate the specifics of their interactions, suggesting a more complex dynamic involving diplomacy, cultural exchange, and potential conflict. 
          """)
+    st.markdown(f'''
+                    <ul class='outline-summary'>
+                        <li>She died in England in 1617, at the young age of 20 or 21.</li>
+                        <li>he is famously known for her interactions with the English colonists at Jamestown.</li>
+                        <li>She died in England in 1617, at the young age of 20 or 21.</li>
+                        <li>he is famously known for her interactions with the English colonists at Jamestown.</li>
+                        <li>She died in England in 1617, at the young age of 20 or 21.</li>
+                        <li>he is famously known for her interactions with the English colonists at Jamestown.</li>
+                    </ul>
+                    ''',
+                unsafe_allow_html=True)
