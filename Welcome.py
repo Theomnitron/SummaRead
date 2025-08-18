d31ed57b9d1d4b0e8c950fdb902a59f430c621f5
Olaitan Toluwanimi Michael <101925362+Theomnitron@users.noreply.github.com>
2025-08-18 04:38:53 +0000

    done

--- Welcome.py
+++ Welcome.py
@@ -11,7 +11,7 @@ st.set_page_config(
                 'Get Help': 'https://www.extremelycoolapp.com/help',
                 'Report a bug': "https://www.extremelycoolapp.com/bug",
                 'About': "# This is a header. This is an *extremely* cool app!"
-            }
+            },
             page_icon= ':material/home'
         )
 
