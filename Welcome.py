c61f0f17c5e61441d92fa4c282ae454c9151e520
Olaitan Toluwanimi Michael <101925362+Theomnitron@users.noreply.github.com>
2025-08-18 02:18:32 +0000

    vertical align

--- Welcome.py
+++ Welcome.py
@@ -22,8 +22,8 @@ render_sidebar_profile()
 # This content will only be shown if the user is logged in
 # if st.session_state.get('logged_in', False):
 
-st.markdown('<h6 style = "text-align: center; padding-bottom: 0px; padding-top: 170px; font-family: Inter; font-size: 13px;">Welcome to</h6>', unsafe_allow_html= True)
-st.markdown('<h2 style = "text-align: center; padding-top: 0px; margin-top: -5px; font-weight: bold; font-size: 50px;">SummaRead!</h2>', unsafe_allow_html= True)
+st.markdown('<h6 style = "text-align: center; padding: 40% 0; font-family: Inter; font-size: 13px;">Welcome to</h6>', unsafe_allow_html= True)
+st.markdown('<h2 style = "text-align: center; padding: 40% 0; margin-top: -5px; font-weight: bold; font-size: 50px;">SummaRead!</h2>', unsafe_allow_html= True)
 
 
 # The "Get Started" button to navigate to the source page
